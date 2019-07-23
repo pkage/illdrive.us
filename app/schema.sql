26724c7b117e3f99d919fb697ea3642b7b2002b5
Paul Horton <pahorton@asu.edu>
2019-07-22 22:57:19 -0700

    +3 to tables

--- app/schema.sql
+++ app/schema.sql
@@ -5,12 +5,28 @@ DROP TABLE IF EXISTS Events;
 DROP TABLE IF EXISTS Cars;
 DROP TABLE IF EXISTS Passengers;
 
-CREATE TABLE Events (
+CREATE TABLE `Events`
+(
+  `id` int PRIMARY KEY,
+  `when` datetime,
+  `name` varchar(200)
 );
 
-CREATE TABLE Campaigns (
+CREATE TABLE `Cars`
+(
+  `id` int PRIMARY KEY,
+  `event_fk` int,
+  `name` varchar(200),
+  `seats` int
 );
 
-CREATE TABLE Tickets (
+CREATE TABLE `Passengers`
+(
+  `id` int PRIMARY KEY,
+  `car_fk` int,
+  `name` varchar(200)
 );
 
+ALTER TABLE `Cars` ADD FOREIGN KEY (`event_fk`) REFERENCES `Events` (`id`);
+
+ALTER TABLE `Passengers` ADD FOREIGN KEY (`car_fk`) REFERENCES `Cars` (`id`);
