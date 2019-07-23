f9129c315451296b1de5d586bb359eee109113ce
Patrick Kage <patrick.r.kage@gmail.com>
2019-07-23 01:18:49 -0700

    Clean up frontend code, add comments and remove logging

--- app/static/edit.js
+++ app/static/edit.js
@@ -1,6 +1,5 @@
 
 const computeCapacities = () => {
-    console.log('computing capacity')
     for (let el of document.querySelectorAll('.cars__cardrop')) {
         if (el.childElementCount >= el.dataset.capacity) {
             el.parentElement.setAttribute('data-full', true)
@@ -14,12 +13,16 @@ window.onload = () => {
     dragula([...document.querySelectorAll('[data-drop]')], {
         revertOnSpill: true,
         accepts: (el, target, source, sibling) => {
-            console.log(target.parentElement.dataset.full)
             return true
         }
     })
     .on('drag', (el) => {
         computeCapacities();
+
+        if (el.parentElement.parentElement.classList.contains('cars__dropcontainer')) {
+            // hack, but keeps the .cars__carfull from showing on the dragstart
+            el.parentElement.parentElement.setAttribute('data-full', false)
+        }
         document.querySelector('.cars').setAttribute('data-dropshown', true)
     })
     .on('dragend', el => {
